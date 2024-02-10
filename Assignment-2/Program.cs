4516006e1562ac03cbe2fb49fe9a442dda6dbd73
Sunil Patel <Spatel4798@conestogac.on.ca>
2024-02-10 18:51:16 -0500

    Add project files.

---
+++ Assignment-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        // Constructor
+        X = x;
+        Y = y;
+    }
+}
