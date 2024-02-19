0fb300ac8b27d5cceb0577b08f747d398be68912
Sunil Patel <156108180+direct2sunny@users.noreply.github.com>
2024-02-19 13:37:05 -0500

    Update Program.cs

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -1,121 +1,231 @@
 using System;
-
-public class Position
+// Main Method
+namespace GemHunters
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    public Position(int x, int y)
+    class Program
     {
-        // Constructor
-        X = x;
-        Y = y;
-    }
-}
-
-public class Player
-{
-    public string Name { get; set; }
-    public Position Position { get; set; }
-    public int GemCount { get; set; }
-
-    public Player(string name, Position position)
-    {
-        Name = name;
-        Position = position;
-        GemCount = 0;
-    }
-
-    public void Move(char direction)
-    {
-        switch (direction)
+        static void Main(string[] args)
         {
-            case 'U': Position.Y--; break;
-            case 'D': Position.Y++; break;
-            case 'L': Position.X--; break;
-            case 'R': Position.X++; break;
+            Game game = new Game();
+            game.Start();
         }
     }
-}
-
-public class Cell
-{
-    public string Occupant { get; set; }
-
-    public Cell(string occupant = "-")
+    // Position Class
+    public class Position
     {
-        Occupant = occupant;
-    }
-}
+        public int X { get; set; }
+        public int Y { get; set; }
 
-public class Board
-{
-    public Cell[,] Grid { get; set; }
-
-    public Board()
-    {
-        Grid = new Cell[6, 6];
-        for (int i = 0; i < 6; i++)
+        public Position(int x, int y)
         {
-            for (int j = 0; j < 6; j++)
+            X = x;
+            Y = y;
+        }
+    }
+    // Player Class
+    public class Player
+    {
+        public string Name { get; set; }
+        public Position Position { get; set; }
+        public int GemCount { get; set; }
+
+        public Player(string name, Position position)
+        {
+            Name = name;
+            Position = position;
+            GemCount = 0;
+        }
+
+        public void Move(char direction)
+        {
+            switch (direction)
             {
-                Grid[i, j] = new Cell();
+                case 'U': Position.Y = Math.Max(Position.Y - 1, 0); break;
+                case 'D': Position.Y = Math.Min(Position.Y + 1, 5); break;
+                case 'L': Position.X = Math.Max(Position.X - 1, 0); break;
+                case 'R': Position.X = Math.Min(Position.X + 1, 5); break;
             }
         }
-        PlaceObstacles();
-        PlaceGems();
     }
-
-    public void Display()
+    // Cell Class
+    public class Cell
     {
-        for (int i = 0; i < 6; i++)
+        public string Occupant { get; set; }
+
+        public Cell(string occupant = "-")
         {
-            for (int j = 0; j < 6; j++)
+            Occupant = occupant;
+        }
+    }
+    // 6x6 Board Class
+    public class Board
+    {
+        public Cell[,] Grid { get; set; }
+
+        public Board()
+        {
+            Grid = new Cell[6, 6];
+            for (int i = 0; i < 6; i++)
             {
-                Console.Write(Grid[i, j].Occupant + " ");
+                for (int j = 0; j < 6; j++)
+                {
+                    Grid[i, j] = new Cell();
+                }
             }
-            Console.WriteLine();
+            PlaceObstacles();
+            PlaceGems();
         }
-    }
 
-    private void PlaceObstacles()
-    {
-        Random rand = new Random();
-        for (int i = 0; i < 10; i++)
+        private void PlaceObstacles()
         {
-            int x, y;
-            do
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
             {
-                x = rand.Next(0, 6);
-                y = rand.Next(0, 6);
-            } while (Grid[y, x].Occupant != "-");
-            Grid[y, x].Occupant = "O";
+                int x, y;
+                do
+                {
+                    x = rand.Next(0, 6);
+                    y = rand.Next(0, 6);
+                } while (Grid[y, x].Occupant != "-");
+                Grid[y, x].Occupant = "O";
+            }
         }
-    }
 
-    private void PlaceGems()
-    {
-        Random rand = new Random();
-        for (int i = 0; i < 5; i++)
+        private void PlaceGems()
         {
-            int x, y;
-            do
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
             {
-                x = rand.Next(0, 6);
-                y = rand.Next(0, 6);
-            } while (Grid[y, x].Occupant != "-");
-            Grid[y, x].Occupant = "G";
+                int x, y;
+                do
+                {
+                    x = rand.Next(0, 6);
+                    y = rand.Next(0, 6);
+                } while (Grid[y, x].Occupant != "-");
+                Grid[y, x].Occupant = "G";
+            }
+        }
+
+        public void Display(Player player1, Player player2)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (i == player1.Position.Y && j == player1.Position.X)
+                    {
+                        Console.Write("P1 ");
+                    }
+                    else if (i == player2.Position.Y && j == player2.Position.X)
+                    {
+                        Console.Write("P2 ");
+                    }
+                    else
+                    {
+                        Console.Write(Grid[i, j].Occupant + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool IsValidMove(Player player, char direction)
+        {
+            int newX = player.Position.X;
+            int newY = player.Position.Y;
+            switch (direction)
+            {
+                case 'U': newY--; break;
+                case 'D': newY++; break;
+                case 'L': newX--; break;
+                case 'R': newX++; break;
+            }
+            return newX >= 0 && newX < 6 && newY >= 0 && newY < 6 && Grid[newY, newX].Occupant != "O";
+        }
+
+        public void CollectGem(Player player)
+        {
+            if (Grid[player.Position.Y, player.Position.X].Occupant == "G")
+            {
+                player.GemCount++;
+                Grid[player.Position.Y, player.Position.X].Occupant = "-";
+            }
         }
     }
-
-    public bool IsValidMove(Player player, char direction)
+    // Game Class
+    public class Game
     {
-        // Implement validation logic here
-        return true; // Placeholder
-    }
+        public Board Board { get; set; }
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        private Player CurrentTurn { get; set; }
+        private int TotalTurns { get; set; }
 
-    public void CollectGem(Player player)
-    {
-        // Implement gem collection logic here
+        public Game()
+        {
+            Board = new Board();
+            Player1 = new Player("P1", new Position(0, 0));
+            Player2 = new Player("P2", new Position(5, 5));
+            CurrentTurn = Player1;
+            TotalTurns = 0;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Gem Hunters!");
+            while (!IsGameOver())
+            {
+                Console.Clear();
+                Board.Display(Player1, Player2);
+                // Displaying the current turn counter
+                Console.WriteLine($"Turn: {TotalTurns + 1}");
+                Console.Write($"{CurrentTurn.Name}'s move (U/D/L/R): ");
+                var input = Console.ReadLine().ToUpper();
+                if (!string.IsNullOrEmpty(input))
+                {
+                    char direction = input[0];
+                    if (Board.IsValidMove(CurrentTurn, direction))
+                    {
+                        CurrentTurn.Move(direction);
+                        Board.CollectGem(CurrentTurn);
+                        SwitchTurn();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid move. Try again.");
+                        Console.ReadKey();
+                    }
+                }
+            }
+            AnnounceWinner();
+        }
+
+        private void SwitchTurn()
+        {
+            CurrentTurn = CurrentTurn == Player1 ? Player2 : Player1;
+            TotalTurns++;
+        }
+
+        private bool IsGameOver()
+        {
+            return TotalTurns == 30;
+        }
+
+        private void AnnounceWinner()
+        {
+            Console.WriteLine($"Game Over. P1 Gems: {Player1.GemCount}, P2 Gems: {Player2.GemCount}");
+            if (Player1.GemCount > Player2.GemCount)
+            {
+                Console.WriteLine("Player 1 wins!");
+            }
+            else if (Player2.GemCount > Player1.GemCount)
+            {
+                Console.WriteLine("Player 2 wins!");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
+        }
     }
 }
